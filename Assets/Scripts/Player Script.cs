11c36b432c09c595518c5d6ad93cd9fb3514df92
GeorgiaDavies-escg <535269@escg.ac.uk>
2025-10-20 09:29:21 +0100

    Project Commit

---
+++ Assets/Scripts/Player Script.cs
@@ -0,0 +1,137 @@
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public GameObject weapon;
+    public LayerMask groundLayer;
+    public Animator anim;
+    bool isGrounded;
+    HelperScript helper;
+
+    void Start()
+    {
+       rb = GetComponent<Rigidbody2D>();
+       groundLayer = LayerMask.GetMask("Ground");
+       helper = gameObject.AddComponent<HelperScript>();
+    }
+
+    void Update()
+    {
+        float xvel, yvel;
+
+        xvel = rb.linearVelocity.x;
+        yvel = rb.linearVelocity.y;
+
+        if (Input.GetKey("a"))                  //Increases velocity to move Left
+        {
+            if(Input.GetKey(KeyCode.LeftControl))
+            {
+                xvel = -6;
+            }
+            else
+            {
+                xvel = -3;
+            }
+        }
+
+        if (Input.GetKey("d"))                  //Increases velocity to move Right
+        {
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                xvel = 6;
+            }
+            else
+            {
+                xvel = 3;
+            }
+        }
+
+        if (((Input.GetKeyDown(KeyCode.Space) || (Input.GetKeyDown("w")))))     //Increases velocity to move upward (Jump) if sprite is touching a box collider
+        {
+            if (isGrounded)
+            {
+                yvel = 5;
+            }
+        }
+
+        if (xvel > 0.1f)
+        {
+            helper.DoFlipObject(true);
+        }
+
+        if (xvel < -0.1f)
+        {
+            helper.DoFlipObject(false);
+        }
+
+        if(xvel >= 0.1f || xvel <= -0.1f)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        if(isGrounded == true)
+        {
+            anim.SetBool("isJumping", false);
+        }
+        else
+        {
+            anim.SetBool("isJumping", true);
+        }
+
+            rb.linearVelocity = new Vector2(xvel, yvel);  //Tells the rigid body to move based on given velocity
+
+        GroundCheck();
+        Shoot();
+    }
+
+    void GroundCheck()
+    {
+        isGrounded = DoRayCollisionCheck();
+    }
+
+    public bool DoRayCollisionCheck()
+    {
+        float rayLength = 0.5f;
+
+
+        RaycastHit2D hit;
+
+        hit = Physics2D.Raycast(transform.position, Vector2.down, rayLength, groundLayer);
+
+        Color hitColor = Color.white;
+
+
+        if (hit.collider != null)
+        {
+            print("Player has collided with Ground layer");
+            hitColor = Color.green;
+        }
+
+        Debug.DrawRay(transform.position, Vector2.down * rayLength, hitColor);
+        return hit.collider;
+    }
+
+
+    void Shoot()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            GameObject clone;
+            clone = Instantiate(weapon, transform.position, transform.rotation);
+
+            Rigidbody2D rb = clone.GetComponent<Rigidbody2D>();
+
+            rb.linearVelocity = transform.right * 15;
+
+            rb.transform.position = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z + 1);
+        }
+
+    } //Shoot a projectile
+
+}
